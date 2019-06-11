532739e9e9e43e0d4bdd14caaa255b4127b24e76
Gulei <gulei8512@163.com>
2019-06-11 23:51:37 +0800

    INIT

---
+++ src/views/CategoryEdit.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="container">
+		<h1>新建分类</h1>
+		<el-form label-width="120px" :model="model" @submit.native.prevent="save">
+			<el-form-item label="名称">
+				<el-input v-model="model.name"></el-input>
+			</el-form-item>
+			<el-form-item label="名称">
+				<el-button type="primary" native-type="submit">保存</el-button>
+			</el-form-item>
+		</el-form>
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			model: {}
+		};
+	},
+	methods: {
+		save() {
+			console.log(1);
+			this.$http.post().then(() => {});
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/CategoryList.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="container">分类列表</div>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
